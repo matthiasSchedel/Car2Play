3303d724d96c73b2fc443c5498195ff5b189ff35
Nick Fode <nickfode@posteo.de>
2017-10-28 12:13:07 +0200

    Fixed install script

--- hostapd-fix/Readme.md
+++
@@ -1,6 +0,0 @@
-# `hostapd` fix
-
-Sometimes `hostapd` cannot be installed.
-The `upgrade-sources.list.sh` adds additional repos that contain the package.
-
-
--- raspi-wifi-hotspot/install-wifi-hotspot.sh
+++ raspi-wifi-hotspot/install-wifi-hotspot.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-
-sudo su
-
 # upgrade raspi
-cp files/sources.list /etc/apt/sources.list
+cp files/source.list /etc/apt/source.list
 apt-get update
-apt-get dist-upgrade
+apt-get -y dist-upgrade
 
 apt-get install dnsmasq hostapd
 
@@ -23,7 +20,7 @@ ifup wlan0
 cp files/dnsmasq.conf /etc/dnsmasq.conf
 cp files/hostapd.conf /etc/hostapd/hostapd.conf
 
-echo "DAEMON_CONF=\"/etc/hostapd/hostapd.conf" >> /etc/default/hostapd
+echo "DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"" >> /etc/default/hostapd
 
 service hostapd start
 service dnsmasq start 
