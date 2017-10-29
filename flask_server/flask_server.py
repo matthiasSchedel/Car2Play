931622daebf745b18d4740e541bab549551586fe
Leo Mehlig <leo@leomehlig.com>
2017-10-29 09:23:39 +0100

    SetWheelSpeed

--- flask_server/flask_server.py
+++ flask_server/flask_server.py
@@ -2,6 +2,9 @@ from flask import Flask
 from AlphaBot2 import AlphaBot2
 from flask import request
 from lib.servo.servo import Servo
+from werkzeug.contrib.cache import SimpleCache
+
+cache = SimpleCache()
 
 app = Flask(__name__)
 
@@ -11,20 +14,20 @@ s = Servo()
 
 @app.route("/forward/")
 def forward():
-    return "forward!"
     Ab.forward()
+    return "forward!"
 
 
 @app.route("/backward/")
 def backward():
-    return "backward!"
     Ab.backward()
+    return "backward!"
 
 
 @app.route("/stop/")
 def stop():
-    return "stop!"
     Ab.stop()
+    return "stop!"
 
 
 @app.route("/left/")
@@ -43,7 +46,11 @@ def right():
 def servoLeftRight():
     position = int(request.args.get("position"))
     speed = int(request.args.get("speed"))
-    s.turnLeftRight(position, speed)
+    current = cache.get("leftright")
+    if (current is None):
+        current = 2450
+    s.turnLeftRight(current, position, speed)
+    cache.set("leftright", position)
     return "servoleftrigth!"
 
 
@@ -51,20 +58,40 @@ def servoLeftRight():
 def servoUpDown():
     position = int(request.args.get("position"))
     speed = int(request.args.get("speed"))
-    s.turnUpDown(position, speed)
+    current = cache.get("updown")
+    if (current is None):
+        current = 3000
+    s.turnUpDown(current, position, speed)
+    cache.set("updown", position)
     return "servoupdown!"
+
 
 @app.route("/servo/center/")
 def servoCenter():
-    s.center()
+    currentUpDown = cache.get("updown")
+    currentLeftRight = current = cache.get("leftright")
+    if (currentLeftRight is None):
+        currentLeftRight = 2450
+    if (currentUpDown is None):
+        currentUpDown = 2450
+    s.center(currentLeftRight, currentUpDown)
+    cache.set("leftright", 1500)
+    cache.set("updown", 2600)
     return "servoupdown!"
 
-@app.route("/setWheelSpeed/")
+
+@app.route("/setWheelSpeed/", methods=["POST"])
 def setWheelSpeed():
-    left = request.args.get('leftWheelSpeed')
-    right = request.args.get('rightWheelSpeed')
-    Ab.setMotor(left, right)
+    left = float(request.get_json()['leftWheelSpeed'])
+    right = float(request.get_json()['rightWheelSpeed'])
+    maximum = float(50)
+    # Ab.setMotor(left, right)
+    Ab.forward()
+    print((maximum * (left / 100)))
+    Ab.setPWMA(maximum * (left / 100))
+    Ab.setPWMB(maximum * (right / 100))
+    return "servoupdown!"
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=True)
