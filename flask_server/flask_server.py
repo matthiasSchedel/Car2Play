f9d81d93499b2b88032c15563ec5c7c420f3667a
Nick Fode <nickfode@posteo.de>
2017-10-29 09:07:39 +0100

    Refactored rest api

--- flask_server/flask_server.py
+++ flask_server/flask_server.py
@@ -2,6 +2,9 @@ from flask import Flask
 from AlphaBot2 import AlphaBot2
 from flask import request
 from lib.servo.servo import Servo
+from werkzeug.contrib.cache import SimpleCache
+
+cache = SimpleCache()
 
 app = Flask(__name__)
 
@@ -11,20 +14,20 @@ s = Servo()
 
 @app.route("/forward/")
 def forward():
-    return "forward!"
     Ab.forward()
+    return "forward!"
 
 
 @app.route("/backward/")
 def backward():
-    return "backward!"
     Ab.backward()
+    return "backward!"
 
 
 @app.route("/stop/")
 def stop():
-    return "stop!"
     Ab.stop()
+    return "stop!"
 
 
 @app.route("/left/")
@@ -40,24 +43,41 @@ def right():
 
 
 @app.route("/servo/leftright/")
-def servoLeftRight():
-    position = int(request.args.get("position"))
-    speed = int(request.args.get("speed"))
-    s.turnLeftRight(position, speed)
-    return "servoleftrigth!"
-
+def servo_leftright():
+    handle_request(request, 0)
+    return "servoleftright!"
 
 @app.route("/servo/updown/")
-def servoUpDown():
+def servo_updown():
+    handle_request(request, 1)
+    return "servoupdown!"
+
+
+def handle_request(request, mode_id):
     position = int(request.args.get("position"))
     speed = int(request.args.get("speed"))
-    s.turnUpDown(position, speed)
-    return "servoupdown!"
+    defaults = s.leftright_range
+    key = "leftright"
+    if (mode_id == 1):
+        key = "updown"
+        defaults = s.updown_range
+
+    current = cache.get(key)
+    if (current is None): current = defaults[1]
+    result = s.run_servo(current, position, speed, mode_id, defaults)
+    cache.set(key, result)
 
 @app.route("/servo/center/")
 def servoCenter():
-    s.center()
+    current_updown = cache.get("updown")
+    current_left_right  = cache.get("leftright")
+    if (current_left_right is None): current_left_right = s.leftright_range[0]
+    if (current_updown is None): current_updown = s.leftright_range[1]
+    s.center(current_left_right, current_updown)
+    cache.set("leftright", 1500)
+    cache.set("updown", 2600)
     return "servoupdown!"
+
 
 @app.route("/setWheelSpeed/")
 def setWheelSpeed():
@@ -67,4 +87,4 @@ def setWheelSpeed():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=True)
